01f6367881a62410fecaba34f8eae5f23cda475f
Wesley1999 <35622526+Wesley1999@users.noreply.github.com>
2021-06-24 20:33:27 +0800

    Update itchat-WXreply.py

--- itchat-WXreply.py
+++ itchat-WXreply.py
@@ -14,11 +14,15 @@ import logging
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-key = ['1a337b641da04c64aa7fd4849a5f713e',
-       '********************************',
-       '********************************',
-       '********************************',
-       '********************************']
+keys = ['c4fae3a2f8394b73bcffdecbbb4c6ac6',
+       '0ca694db371745668c28c6cb0a755587',
+       '7855ce1ebd654f31938505bb990616d4',
+       '5945954988d24ed393f465aae9be71b9',
+       '1a337b641da04c64aa7fd4849a5f713e',
+       '8edce3ce905a4c1dbb965e6b35c3834d',
+       'eb720a8970964f3f855d863d24406576',
+       '1107d5601866433dba9599fac1bc0083',
+       '71f28bf79c820df10d39b4074345ef8c']
 nickName = u'小兔几' # 这里是昵称或群昵称，用于检测群聊是否被艾特
 
 
@@ -26,7 +30,7 @@ nickName = u'小兔几' # 这里是昵称或群昵称，用于检测群聊是否
 def get_response(msg):
     key_index = 0
     text = '亲爱的，当天请求次数已用完。'
-    while text == '亲爱的，当天请求次数已用完。' and key_index <= 4:
+    while text == '亲爱的，当天请求次数已用完。' and key_index < len(keys):
         # 构造要发送给服务器的数据
         apiUrl = 'http://www.tuling123.com/openapi/api'
         data = {'key': key[key_index], 'info': msg, 'userid' : 'wechat-robot'}
